3545d50310f706d08be7e2dd5534b1d41f274ad9
Daniel Feldroy <daniel@feldroy.com>
2021-02-02 15:58:06 -0800

    More useful index

--- fetch.py
+++ fetch.py
@@ -38,10 +38,10 @@ for link in links:
     long_url = link['long_url']
     key = key_maker(link['link'])
     final[key] = long_url
-    redirects.append(f'{key}    {long_url}')
+    redirects.append((key, long_url))
     for bitlink in link['custom_bitlinks']:
         final[key_maker(bitlink)] = long_url
-        redirects.append(f'{key_maker(bitlink)}    {long_url}')
+        redirects.append((key_maker(bitlink),long_url))
 
 
 data['ppp'] = 'https://www.feldroy.com/products/practical-python-projects'
@@ -52,10 +52,16 @@ with open('data.json', 'w') as f:
     data = json.dumps(final, indent=2)
     f.write(data)
 
-redirects.append('ppp    https://www.feldroy.com/products/practical-python-projects')
+redirects.append(('ppp','https://www.feldroy.com/products/practical-python-projects'))
 
 with open('public/_redirects', 'w') as f:
-    for redirect in redirects:
-        print(redirect)
-        f.write(f'/{redirect}\n')
+    for link in redirects:
+        print(link)
+        f.write(f'/{link[0]}    {link[1]}\n')
 
+with open("public/index.html", "w") as f:
+    f.write("<h1>Feldroy Links</h1>")
+    f.write('<ol>')
+    for link in redirects:
+        f.write(f'<li><a href="{link[1]}">/{link[0]}</a></li>')
+    f.write('</ol>')
