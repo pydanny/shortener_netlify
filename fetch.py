4a110d389c6484e270ca543f30746bca119889e4
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2021-06-13 14:35:38 -0700

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,68 +1,68 @@
-###Super sloppy code - don't laugh!
+# ###Super sloppy code - don't laugh!
 
-import requests
-import json
+# import requests
+# import json
 
-import settings
-
-
+# import settings
 
 
-links = []
 
-r = requests.get(f'{settings.BASE_URL}/groups/{settings.GROUP_GUID}/bitlinks', headers=settings.HEADERS)
-data = r.json()
 
-links += data['links']
+# links = []
 
-for i in range(7):
-    if data['pagination']['next'] == '':
-        break
-    r = requests.get(data['pagination']['next'], headers=settings.HEADERS)
-    data = r.json()
-    links += data['links']
+# r = requests.get(f'{settings.BASE_URL}/groups/{settings.GROUP_GUID}/bitlinks', headers=settings.HEADERS)
+# data = r.json()
+
+# links += data['links']
+
+# for i in range(7):
+#     if data['pagination']['next'] == '':
+#         break
+#     r = requests.get(data['pagination']['next'], headers=settings.HEADERS)
+#     data = r.json()
+#     links += data['links']
     
 
 
-# print(links)
-# print(len(links))
-def key_maker(value):
-    result = value.rsplit('/', 1)[-1]
-    # print('result:', result)
-    return result.lower()
+# # print(links)
+# # print(len(links))
+# def key_maker(value):
+#     result = value.rsplit('/', 1)[-1]
+#     # print('result:', result)
+#     return result.lower()
 
-final = {}
-redirects = []
+# final = {}
+# redirects = []
 
-for link in links:
-    long_url = link['long_url']
-    key = key_maker(link['link'])
-    final[key] = long_url
-    redirects.append((key, long_url))
-    for bitlink in link['custom_bitlinks']:
-        final[key_maker(bitlink)] = long_url
-        redirects.append((key_maker(bitlink),long_url))
-
-
-data['ppp'] = 'https://www.feldroy.com/products/practical-python-projects'
+# for link in links:
+#     long_url = link['long_url']
+#     key = key_maker(link['link'])
+#     final[key] = long_url
+#     redirects.append((key, long_url))
+#     for bitlink in link['custom_bitlinks']:
+#         final[key_maker(bitlink)] = long_url
+#         redirects.append((key_maker(bitlink),long_url))
 
 
-with open('data.json', 'w') as f:
+# data['ppp'] = 'https://www.feldroy.com/products/practical-python-projects'
 
-    data = json.dumps(final, indent=2)
-    f.write(data)
 
-redirects.append(('ppp','https://www.feldroy.com/products/practical-python-projects'))
+# with open('data.json', 'w') as f:
 
-with open('public/_redirects', 'w') as f:
-    for link in redirects:
-        print(link)
-        f.write(f'/{link[0]}    {link[1]}\n')
+#     data = json.dumps(final, indent=2)
+#     f.write(data)
 
-with open("public/index.html", "w") as f:
-    f.write("<h1>Feldroy Shortened Links</h1>")
-    f.write("<p>Many of these exist because the # sign breaks certain PDF readers on files rendered from LaTeX.</p>")
-    f.write('<ol>')
-    for link in redirects:
-        f.write(f'<li><a href="{link[1]}">/{link[0]}</a></li>')
-    f.write('</ol>')
+# redirects.append(('ppp','https://www.feldroy.com/products/practical-python-projects'))
+
+# with open('public/_redirects', 'w') as f:
+#     for link in redirects:
+#         print(link)
+#         f.write(f'/{link[0]}    {link[1]}\n')
+
+# with open("public/index.html", "w") as f:
+#     f.write("<h1>Feldroy Shortened Links</h1>")
+#     f.write("<p>Many of these exist because the # sign breaks certain PDF readers on files rendered from LaTeX.</p>")
+#     f.write('<ol>')
+#     for link in redirects:
+#         f.write(f'<li><a href="{link[1]}">/{link[0]}</a></li>')
+#     f.write('</ol>')
